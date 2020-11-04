e28f215a30b473ac2a3a924392375a74f5a44e56
zhouqiang <zhouqiang@powersi.com>
2020-11-04 08:52:18 +0800

    创建项目

---
+++ archar-server/src/main/java/fgo/xiaox/archar/ArcharApplication.java
@@ -0,0 +1,13 @@
+package fgo.xiaox.archar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcharApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArcharApplication.class, args);
+    }
+
+}
---
+++ archar-server/src/test/java/fgo/xiaox/archar/ArcharApplicationTests.java
@@ -0,0 +1,13 @@
+package fgo.xiaox.archar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcharApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
